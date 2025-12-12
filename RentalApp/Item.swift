334e78f6ced92d4d4c496fe7f85b436e0a0a0e62
Andre Mazidowski <andremazidowski@gmail.com>
2025-12-12 15:53:36 +0100

    Initial Commit

---
+++ RentalApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  RentalApp
+//
+//  Created by Andre on 12.12.25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ RentalAppTests/RentalAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RentalAppTests.swift
+//  RentalAppTests
+//
+//  Created by Andre on 12.12.25.
+//
+
+import Testing
+@testable import RentalApp
+
+struct RentalAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
